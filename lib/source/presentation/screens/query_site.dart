a6fd3eaaca5bb10cd4fe9ac94b7ffc6c0cd3cb35
p-jura <themadman@o2.pl>
2023-05-02 22:38:15 +0200

    adds second page layout

--- lib/source/presentation/screens/query_site.dart
+++ lib/source/presentation/screens/query_site.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:fuksiarz_imitation/source/presentation/widgets/main_site_header_widget.dart';
 
+const Color BORDERCOLOR = Color.fromARGB(255, 227, 232, 238);
+const Color BACKGROUNDCOLOR = Color.fromARGB(255, 248, 248, 248);
+const Color DEEPBORDERCOLOR = Color.fromARGB(255, 196, 196, 196);
+const Color DEEPBACKGROUND = Color.fromARGB(255, 198, 40, 40);
+
+const String EARLYPAYOUT = 'EARLY PAYOUT';
+const String POLECANE = 'POLECANE:';
+const String POKAZWSZYSTKIE = 'POKAŻ WSZYSTKIE';
+
+const String FIRSTIMAGEPATH = 'assets/images/first_image.png';
+const String SECONDIMAGEPATH = 'assets/images/second_image.png';
+const String THIRDIMAGEPATH = 'assets/images/third_image.png';
+const String FOURTHIMAGEPATH = 'assets/images/fourth_image.png';
+const String FIFTHIMAGEPATH = 'assets/images/fifth_image.png';
+
 class QuerySite extends StatefulWidget {
   const QuerySite({super.key});
 
@@ -16,7 +31,7 @@ class _QuerySiteState extends State<QuerySite> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 198, 40, 40),
+      backgroundColor: DEEPBACKGROUND,
       body: Column(
         children: [
           Expanded(
@@ -31,40 +46,107 @@ class _QuerySiteState extends State<QuerySite> {
                   topLeft: Radius.circular(40),
                   topRight: Radius.circular(40),
                 ),
-                color: Color.fromARGB(255, 248, 248, 248),
+                color: BACKGROUNDCOLOR,
               ),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 40,
-                      horizontal: 20,
-                    ),
-                    child: TextFormField(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 40,
+                  horizontal: 20,
+                ),
+                child: Column(
+                  children: [
+                    TextFormField(
                       controller: _controller,
                       keyboardType: TextInputType.text,
+                      style: const TextStyle(
+                        fontSize: 13,
+                        fontWeight: FontWeight.bold,
+                      ),
                       decoration: InputDecoration(
-                        iconColor: Colors.white,
+                        filled: true,
+                        fillColor: Colors.white,
                         enabledBorder: OutlineInputBorder(
                           borderSide: const BorderSide(
-                            color: Color.fromARGB(255, 227, 232, 238),
+                            color: BORDERCOLOR,
                           ),
                           borderRadius: BorderRadius.circular(5),
                         ),
                         focusedBorder: OutlineInputBorder(
                           borderSide: const BorderSide(
-                            color: Color.fromARGB(255, 227, 232, 238),
+                            color: BORDERCOLOR,
                           ),
                           borderRadius: BorderRadius.circular(5),
                         ),
-                        label: const Icon(
+                        prefixIcon: const Icon(
                           Icons.arrow_back_ios_new,
-                          color: Color.fromARGB(255, 227, 232, 238),
+                          color: DEEPBORDERCOLOR,
+                          size: 13,
                         ),
                       ),
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 30),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text(
+                          POLECANE,
+                          style: TextStyle(
+                              fontSize: 12, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          POKAZWSZYSTKIE,
+                          style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                              color: DEEPBACKGROUND),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SizedBox(
+                          height: 80,
+                          child: Image.asset(
+                            FIRSTIMAGEPATH,
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 80,
+                          child: Image.asset(
+                            SECONDIMAGEPATH,
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 80,
+                          child: Image.asset(
+                            THIRDIMAGEPATH,
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 80,
+                          child: Image.asset(
+                            FOURTHIMAGEPATH,
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 80,
+                          child: Image.asset(
+                            FIFTHIMAGEPATH,
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),
