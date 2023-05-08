5eb8a4a229d9b1a235b282b8b96ee41a568bf630
p-jura <themadman@o2.pl>
2023-05-08 13:13:00 +0200

    changing loading indicator on main site part 2

--- lib/source/presentation/widgets/main_site/main_site_events_view_widget.dart
+++ lib/source/presentation/widgets/main_site/main_site_events_view_widget.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:fuksiarz_imitation/source/get_it_instance.dart'
-    as get_it_instance;
-import 'package:fuksiarz_imitation/source/presentation/bloc/all_categories_cubit/all_categories_events_cubit_cubit.dart';
-import 'package:fuksiarz_imitation/source/presentation/widgets/loading_widget.dart';
+
 import 'package:fuksiarz_imitation/source/presentation/widgets/main_site/events_view_widgets/expanded_list_element.dart';
-import 'package:fuksiarz_imitation/core/fixtures/fixtures.dart' as constants;
 
 class MainSiteEventsViewWidget extends StatelessWidget {
-  const MainSiteEventsViewWidget({required this.categoriesMappedWithEvents, super.key,});
+  const MainSiteEventsViewWidget({
+    required this.categoriesMappedWithEvents,
+    super.key,
+  });
   final Map<int, Map<String, dynamic>> categoriesMappedWithEvents;
   @override
   Widget build(BuildContext context) {
+    categoriesMappedWithEvents.remove(0);
     return Expanded(
       child: ListView.builder(
         shrinkWrap: true,
